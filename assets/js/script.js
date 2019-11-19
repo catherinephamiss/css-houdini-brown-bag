62651d740372aa87681a27bf30c8731538f0c309
Catherine Pham <56605191+catherinephamiss@users.noreply.github.com>
2019-11-19 09:25:38 -0800

    worklet paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 
 // add worklets below
 if ('paintWorklet' in CSS) {
-    CSS.paintWorklet.addModule('./worklets/magical-paint-worklet.js');
+    CSS.paintWorklet.addModule('assets/js/worklets/magical-paint-worklet.js');
 } else {
     document.querySelector('body').classList.add('no-paintapi-support');
 }
 
 if ('animationWorklet' in CSS) {
-    CSS.animationWorklet.addModule('./worklets/magical-animation-worklet.js');
+    CSS.animationWorklet.addModule('assets/js/worklets/magical-animation-worklet.js');
 } else {
     document.querySelector('body').classList.add('no-paintapi-support');
 }
 
 if ('layoutWorklet' in CSS) {
-    CSS.layoutWorklet.addModule('./worklets/magical-layout-worklet.js');
+    CSS.layoutWorklet.addModule('assets/js/worklets/magical-layout-worklet.js');
 } else {
     document.querySelector('body').classList.add('no-paintapi-support');
 }
