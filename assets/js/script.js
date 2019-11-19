112b4099351684434940e5f4e1de4073cc9af7b8
Catherine Pham <56605191+catherinephamiss@users.noreply.github.com>
2019-11-19 11:05:27 -0800

    pacman kind of working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
 'use strict';
 
+// register new css properties
+CSS.registerProperty({
+    name: '--magic-color-one',
+    syntax: '<color>',
+    inherits: false,
+    initialValue: 'gray'
+});
+CSS.registerProperty({
+    name: '--magic-color-two',
+    syntax: '<color>',
+    inherits: false,
+    initialValue: 'lightgray',
+});
+
 // add worklets below
 if ('paintWorklet' in CSS) {
     CSS.paintWorklet.addModule('assets/js/worklets/magical-paint-worklet.js');
 } else {
-    document.querySelector('body').classList.add('no-paintapi-support');
+    console.log("no paint api support");
+    document.querySelector('html').classList.add('no-paintapi-support');
 }
 
-if ('animationWorklet' in CSS) {
-    CSS.animationWorklet.addModule('assets/js/worklets/magical-animation-worklet.js');
-} else {
-    document.querySelector('body').classList.add('no-paintapi-support');
-}
+// if ('animationWorklet' in CSS) {
+//     CSS.animationWorklet.addModule('assets/js/worklets/magical-animation-worklet.js');
+// } else {
+//     document.querySelector('html').classList.add('no-paintapi-support');
+// }
 
-if ('layoutWorklet' in CSS) {
-    CSS.layoutWorklet.addModule('assets/js/worklets/magical-layout-worklet.js');
-} else {
-    document.querySelector('body').classList.add('no-paintapi-support');
-}
+// if ('layoutWorklet' in CSS) {
+//     CSS.layoutWorklet.addModule('assets/js/worklets/magical-layout-worklet.js');
+// } else {
+//     document.querySelector('html').classList.add('no-paintapi-support');
+// }
