dd6f41013beae8c1da73de9a92d18fce99a5cbff
Catherine Pham <56605191+catherinephamiss@users.noreply.github.com>
2019-12-03 11:41:23 -0800

    lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,56 @@
 'use strict';
 
 // register new css properties
-CSS.registerProperty({
-    name: '--pacman-color-one',
-    syntax: '<color>',
-    inherits: true,
-    initialValue: 'gray'
-}, {
-    name: '--pacman-color-two',
-    syntax: '<color>',
-    inherits: true,
-    initialValue: 'lightgray',
-}, {
-//     name: '--line-color',
-//     syntax: '<color>',
-//     inherits: true,
-//     initialValue: 'black'
-// }, {
-//     name: '--line-thickness',
-//     syntax: '<length>',
-//     inherits: true,
-//     initialValue: '2px'
-// }, {
-    name: '--hover',
-    syntax: '<color>',
-    inherits: true,
-    initialValue: 'lightpurple'
-});
+if('registerProperty' in CSS) {
+    CSS.registerProperty({
+        name: '--pacman-color-one',
+        syntax: '<color>',
+        inherits: true,
+        initialValue: 'gray'
+    }, {
+        name: '--pacman-color-two',
+        syntax: '<color>',
+        inherits: true,
+        initialValue: 'lightgray',
+    });
+    CSS.registerProperty({
+        name: '--hover',
+        syntax: '<color>',
+        inherits: true,
+        initialValue: 'transparent'
+    });
+    CSS.registerProperty({
+        name: '--line-color',
+        syntax: '<color>',
+        inherits: true,
+        initialValue: 'blue'
+    });
+    CSS.registerProperty({
+        name: '--line-width',
+        syntax: '<length>',
+        inherits: true,
+        initialValue: '10px'
+    })
+} else {
+    document.querySelector('html').classList.add('no-houdini-support');
+}
 
 // add worklets below
 if ('paintWorklet' in CSS) {
     CSS.paintWorklet.addModule('assets/js/worklets/pacman-paint-worklet.js');
-    // CSS.paintWorklet.addModule('assets/js/worklets/headings-paint-worklet.js');
+    CSS.paintWorklet.addModule('assets/js/worklets/headings-paint-worklet.js');
 } else {
-    document.querySelector('html').classList.add('no-paintapi-support');
+    document.querySelector('html').classList.add('no-houdini-support');
 }
 
 // if ('animationWorklet' in CSS) {
 //     CSS.animationWorklet.addModule('assets/js/worklets/magical-animation-worklet.js');
 // } else {
-//     document.querySelector('html').classList.add('no-animationapi-support');
+//     document.querySelector('html').classList.add('no-houdini-support');
 // }
 
 // if ('layoutWorklet' in CSS) {
 //     CSS.layoutWorklet.addModule('assets/js/worklets/magical-layout-worklet.js');
 // } else {
-//     document.querySelector('html').classList.add('no-layoutapi-support');
+//     document.querySelector('html').classList.add('no-houdini-support');
 // }
--- assets/js/worklets/headings-paint-worklet.js
+++ assets/js/worklets/headings-paint-worklet.js
@@ -4,12 +4,28 @@ if (typeof registerPaint !== 'undefined') {
 
     class HeadingUnderline {
         static get inputProperties() {
-            return ['--heading-color', '--heading-thickness'];
+            return ['--line-color', '--line-width'];
         }
 
         paint(ctx, geometry, properties) {
-            const color = properties.get('--heading-color');
-            const thickness = properties.get('--heading-thickness');
+            const color = properties.get('--line-color');
+            const thickness = properties.get('--line-width');
+            ctx.strokeStyle = color;
+            ctx.lineWidth = thickness;
+
+            const yStart = Math.floor(Math.random() * geometry.height -1);
+            const xEnd = geometry.width;
+            const yEnd = Math.floor(Math.random() * geometry.height -1);
+
+            console.log("yStart:", yStart)
+            console.log("xEnd:", xEnd)
+            console.log("yEnd:", yEnd)
+
+            ctx.beginPath();
+            ctx.moveTo(0, yStart);
+            ctx.lineTo(xEnd, yEnd);
+            ctx.closePath();
+            ctx.stroke();
         }
 
     }
