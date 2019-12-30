76356206378a2cf7b4d1b1783989dcfdde244dbd
Catherine Pham <catherinepham@isoftstone.com>
2019-12-30 10:52:32 -0800

    rm extra js in masonry

--- examples/layout - masonry/masonry - with houdini/assets/layout.js
+++ examples/layout - masonry/masonry - with houdini/assets/layout.js
@@ -1,10 +1,10 @@
-if (location.protocol === 'http:' && location.hostname !== 'localhost')
-    location.protocol = 'https:';
-document.querySelectorAll('div').forEach(elem => {
-    const t = elem.textContent;
-// Cut out a random amount of text, but keep at least 10 characters
-elem.textContent = t.slice(0, Math.floor(Math.random() * (t.length - 10) + 10));
-})
+// if (location.protocol === 'http:' && location.hostname !== 'localhost')
+//     location.protocol = 'https:';
+// document.querySelectorAll('div').forEach(elem => {
+//     const t = elem.textContent;
+// // Cut out a random amount of text, but keep at least 10 characters
+// elem.textContent = t.slice(0, Math.floor(Math.random() * (t.length - 10) + 10));
+// })
 if ('layoutWorklet' in CSS) {
     CSS.layoutWorklet.addModule('masonry.js');
 } else {
